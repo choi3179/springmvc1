87685d566eede7f4afee492138396dd40980829d
choi <jacobis@naver.com>
2023-07-10 00:18:55 +0900

    [ADD] 07.10 spring mvc 서블릿 테스트 생성

---
+++ src/main/java/hello/servlet1/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet1.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//  name : 서블릿 이름 // urlPatterns : URL 매핑
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + req);
+        System.out.println("response = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hello " + username);
+
+    }
+}
