a90a14c14e64956cdc8a9ddfe7b370eb92b9d83e
choi <jacobis@naver.com>
2023-07-12 00:22:45 +0900

    [ADD] 07.12 HTTP 요청 데이터 3가지 방법 테스트

---
+++ src/main/java/hello/servlet1/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet1.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//  JSON 형식 파싱하기 위한 객체
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
---
+++ src/main/java/hello/servlet1/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,46 @@
+package hello.servlet1.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet1.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * http://localhost:8080/request-body-json
+ *
+ * JSON 형식 전송
+ * content-type: application/json
+ * message body: {"username": "hello", "age": 20}
+ *
+ */
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    /*
+        JSON 결과를 파싱해서 사용할 수 있는 자바 객체로 변환하려면 Jackson, Gson 같은 변환 라이브러리 추가 필요
+        스프링 부트로 Spring MVC를 선택하면 Jackson 라이브러리의 ObjectMapper를 기본적으로 제공한다.
+     */
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);     // 수신한 JSON 데이터를 자바 객체로 변환
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
